562308a427e81fbff53bf26efebc4c537306bc85
JULIANA ANDREA CARDONA HENAO <julihno17@gmail.com>
2026-08-27 19:31:56 -0500

    Knigh

---
+++ horsesinconflict/Knigh.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace horsesinconflict
+{
+    public class Knigh
+    {
+        public string RawPosition { get; }
+        public int File { get; }
+        public int Rank { get; }
+
+        public Knigh(string position)
+        {
+            RawPosition = position.Trim().ToUpper();
+            if (RawPosition.Length >= 2)
+            {
+                if (char.IsLetter(RawPosition[0]))
+                {
+                    File = RawPosition[0] - 'A' + 1;
+                    Rank = int.Parse(RawPosition[1].ToString());
+                }
+                else
+                {
+                    Rank = int.Parse(RawPosition[0].ToString());
+                    File = RawPosition[1] - 'A' + 1;
+                }
+            }
+        }
+        public string FormattedPosition => $"{Rank}{(char)('A' + File - 1)}";
+
+        public bool Attacks(Knigh other)
+        {
+            int deltaFile = Math.Abs(File - other.File);
+            int deltaRank = Math.Abs(Rank - other.Rank);
+
+            return (deltaFile == 1 && deltaRank == 2) || (deltaFile == 2 && deltaRank == 1);
+        }
+    }
+}
---
+++ horsesinconflict/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace horsesinconflict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese ubicación de los caballos: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input)) return;
+
+            string[] rawKnights = input.Split(',');
+            List<Knigh> knights = rawKnights.Select(k => new Knigh(k)).ToList();
+
+            foreach (var current in knights)
+            {
+                List<string> conflicts = new List<string>();
+                var otherKnights = knights.AsEnumerable().Reverse();
+
+                foreach (var other in otherKnights)
+                {
+                    if (current != other && current.Attacks(other))
+                    {
+                        conflicts.Add($"Conflicto con {other.FormattedPosition}");
+                    }
+                }
+
+                if (conflicts.Count > 0)
+                {
+                    Console.WriteLine($"Analizando Caballo en {current.FormattedPosition} => {string.Join("\t\t", conflicts)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Analizando Caballo en {current.FormattedPosition} =>");
+                }
+            }
+        }
+    }
+}
